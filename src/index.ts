b34e06e303811d56342b5f479fcacb842dfafd85
Victor <ua6em@yandex.ru>
2022-05-02 15:20:30 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,12 @@ export const boards = {
     timeout: 400,
     baudRate: 115200,
   } as Board,
+   328PBminiCore: {
+    signature: Buffer.from([0x1e, 0x95, 0x16]),
+    pageSize: 128,
+    timeout: 400,
+    baudRate: 115200,
+  } as Board,
 }
 
 const noop = (callback: () => void) => callback()
